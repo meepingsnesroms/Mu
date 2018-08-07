6093db40b4ce500c3d38739b17269d2e277da306
meepingsnesroms <guicrith@gmail.com>
2018-08-07 11:34:03 -0700

    Cleaned up some bugs, found some new ones

--- qtBuildSystem/Mu/main.cpp
+++ qtBuildSystem/Mu/main.cpp
@@ -9,7 +9,12 @@ int main(int argc, char* argv[]){
 
    a.setOrganizationDomain("meepingsnesroms.github.com");
    a.setApplicationName("Mu");
+
+#if defined(Q_OS_ANDROID) || defined(Q_OS_IOS)
+   w.showFullScreen();
+#else
    w.show();
+#endif
 
    return a.exec();
 }
