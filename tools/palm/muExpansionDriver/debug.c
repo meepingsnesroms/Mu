20400675fafdd61d77a333f64485ff13a61b3940
meepingsnesroms <guicrith@gmail.com>
2019-04-02 21:02:23 -0700

    Expose memory watch API to MuExpDriver

--- tools/palm/muExpansionDriver/debug.c
+++ tools/palm/muExpansionDriver/debug.c
@@ -1,4 +1,5 @@
 #include "sdkPatch/PalmOSPatched.h"
+#include <stdint.h>
 #include <stdarg.h>
 
 #include "palmGlobalDefines.h"
@@ -19,3 +20,18 @@ void debugLog(const char* format, ...){
    va_end(va);
 }
 #endif
+
+uint16_t setWatchRegion(uint32_t address, uint32_t size, uint8_t type){
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_VALUE), type);
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_SRC), address);
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_SIZE), size);
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_CMD), CMD_DEBUG_WATCH);
+   
+   return readArbitraryMemory32(EMU_REG_ADDR(EMU_VALUE));
+}
+
+void clearWatchRegion(uint16_t index){
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_VALUE), 0);
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_SRC), index);
+   writeArbitraryMemory32(EMU_REG_ADDR(EMU_CMD), CMD_DEBUG_WATCH);
+}
--- tools/palm/muExpansionDriver/debug.h
+++ tools/palm/muExpansionDriver/debug.h
@@ -1,12 +1,22 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <stdint.h>
 #include <stdarg.h>
+
+enum{
+   MEMORY_WATCH_NONE = 0,
+   MEMORY_WATCH_CODE,
+   MEMORY_WATCH_DATA
+};
 
 #ifdef DEBUG
 void debugLog(const char* format, ...);
 #else
 #define debugLog(...)
 #endif
+
+uint16_t setWatchRegion(uint32_t address, uint32_t size, uint8_t type);
+void clearWatchRegion(uint16_t index);
 
 #endif
