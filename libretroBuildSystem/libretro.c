51e405fc84f7bb628f629d604017dd379392998e
meepingsnesroms <guicrith@gmail.com>
2019-06-08 20:43:44 -0700

    Disable OpenMP on libretro for now
    Causing way too many issues for the benefit

--- libretroBuildSystem/libretro.c
+++ libretroBuildSystem/libretro.c
@@ -242,7 +242,7 @@ void retro_set_environment(retro_environment_t cb){
    struct retro_input_descriptor input_desc[] = {
       { 0, RETRO_DEVICE_ANALOG, RETRO_DEVICE_INDEX_ANALOG_LEFT, RETRO_DEVICE_ID_ANALOG_X, "Touchscreen Mouse X" },
       { 0, RETRO_DEVICE_ANALOG, RETRO_DEVICE_INDEX_ANALOG_LEFT, RETRO_DEVICE_ID_ANALOG_Y, "Touchscreen Mouse Y" },
-      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_R,                             "Touchscreen Mouse Click" },
+      { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_R,                              "Touchscreen Mouse Click" },
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_UP,                             "Dpad Up" },
       { 0, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_DOWN,                           "Dpad Down" },
 #if defined(EMU_SUPPORT_PALM_OS5)
