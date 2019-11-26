4b836d98dbd6a5c8e58359e499258ac3372b4f57
meepingsnesroms <guicrith@gmail.com>
2019-11-26 10:31:16 -0800

    Update dbvz.c

--- src/dbvz.c
+++ src/dbvz.c
@@ -82,6 +82,7 @@ void dbvzLcdRender(void){
    height = FAST_MIN(height, dbvzFramebufferHeight);
 
    //TODO: cursor not implemented, not that anything will use a hardware terminal cursor on Palm OS
+   //m500 ROM I am using has the hadware feature bit for inverting color when backlight is on disabled, the backlight does not invert the colors even though HW supports it
 
    switch(bitsPerPixel){
       case 1:
@@ -436,6 +437,7 @@ uint8_t dbvzGetRegister8(uint32_t address){
 
       //LCD controller
       case LPICF:
+      case LPOLCF:
 
       //port d special functions
       case PDPOL:
